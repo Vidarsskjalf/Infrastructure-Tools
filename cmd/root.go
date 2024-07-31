978c1d239006828138bd4140905bc482b2e21e86
Jerrod Havig <secure.havig@gmail.com>
2024-07-30 22:16:36 -0700

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"infraTools/cmd/activeDirectory"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "infraTools",
+	Short: "A brief description of your application",
+	Long: `
+	Infrastructure Tools
+
+infraTools is a robust command-line interface (CLI) utility designed specifically for our infrastructure team.
+It provides a comprehensive toolbox for managing and automating various infrastructure and user services tasks.
+This tool streamlines workflows, enhances productivity,and reduces the complexity of routine operations.`,
+
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func addSubcommandPalettes() {
+	rootCmd.AddCommand(activeDirectory.ActiveDirectoryCmd)
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.infraTools.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	addSubcommandPalettes()
+}
+
+
